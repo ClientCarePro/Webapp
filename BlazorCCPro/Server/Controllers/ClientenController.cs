b961b23b42f8b7b337abd54957f17a0cdf694771
ClientCarePro <development@clientcarepro.nl>
2023-06-17 00:15:04 +0200

    master versie

---
+++ BlazorCCPro/Server/Controllers/ClientenController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorCCPro.Server.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ClientenController : ControllerBase
+	{
+		private static List<Clienten> clientens = new List<Clienten>
+		{
+			new Clienten
+			{
+				Id = 1,
+				Voornaam = "MohammedA",
+				Achternaam = "Bouhkari",
+				Geboortedatum = "31-08-1988",
+				Straatnaam = "Jan van Gentstraat",
+				Huisnummer = "122F",
+				Woonplaats = "Badhoevendorp",
+				Begeleidwonen = true,
+				AmbulanteBegeleiding = false,
+				GeindiceerdeZZPRange = "ZZP1",
+				Actief = true,
+				Aanmelddatum = "01-06-2023",
+				Afmelddatum = "",
+				AantalRapportages = 0
+			},
+			new Clienten
+			{
+				Id = 2,
+				Voornaam = "Klaasje",
+				Achternaam = "van Bommel",
+				Geboortedatum = "20-04-1999",
+				Straatnaam = "Jan van Gentstraat",
+				Huisnummer = "122-F",
+				Woonplaats = "Badhoevendorp",
+				Begeleidwonen = true,
+				AmbulanteBegeleiding = false,
+				GeindiceerdeZZPRange = "ZZP-1",
+				Actief = true,
+				Aanmelddatum = "01-06-2023",
+				Afmelddatum = "",
+				AantalRapportages = 1
+			},
+			new Clienten
+			{
+				Id = 3,
+				Voornaam = "Johan",
+				Achternaam = "Verstraten",
+				Geboortedatum = "01-04-2003",
+				Straatnaam = "Jan van Gentstraat",
+				Huisnummer = "123-K",
+				Woonplaats = "Badhoevendorp",
+				Begeleidwonen = false,
+				AmbulanteBegeleiding = false,
+				GeindiceerdeZZPRange = "ZZP-3",
+				Actief = true,
+				Aanmelddatum = "01-06-2023",
+				Afmelddatum = "",
+				AantalRapportages = 3
+			}
+		};
+
+		[HttpGet]
+		public async Task<ActionResult<List<Clienten>>> GetClient()
+		{
+			return Ok(clientens);
+		}
+
+	}
+}
---
+++ BlazorCCPro/Shared/Clienten.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorCCPro.Shared
+{
+    public class Clienten
+    {
+        public int Id { get; set; }
+                    
+        public string Voornaam { get; set; } = string.Empty;
+
+        public string Achternaam { get; set; } = string.Empty;
+
+        public string Geboortedatum { get; set; } = string.Empty;
+
+        public string Straatnaam { get; set; } = string.Empty;
+
+        public string Huisnummer { get; set; } = string.Empty;
+
+        public string Woonplaats { get; set; } = string.Empty;
+
+        public bool Begeleidwonen { get; set; } = false;
+        
+        public bool AmbulanteBegeleiding { get; set; } = false;
+
+        public string GeindiceerdeZZPRange { get; set; } = string.Empty;
+
+        public string Aanmelddatum { get; set; } = string.Empty;
+
+        public bool Actief { get; set; } = false;
+
+        public string Afmelddatum { get; set ; } = string.Empty;    
+
+        public int AantalRapportages { get; set; } = 0; 
+
+        
+
+
+    }
+}
+
