9c8dff23c61601484c1bb18b8c00921119596ac6
ClientCarePro <development@clientcarepro.nl>
2023-06-30 15:19:37 +0200

    Versie 2

---
+++ BlazorCCPro/Client/Shared/ClientenOverzicht.razor.cs
@@ -0,0 +1,26 @@
+﻿using BlazorCCPro.Shared;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Net.Http.Json;
+
+
+namespace BlazorCCPro.Client.Shared
+{
+	public partial class ClientenOverzicht
+
+	{
+
+		private static List<Clienten> clientens = new List<Clienten>();
+		
+			protected override async Task OnInitializedAsync()
+		{
+			var result = await Http.GetFromJsonAsync<List<Clienten>>("api/Clienten");
+			if (result != null)
+			{
+				clientens = result;
+			}
+		}
+
+		
+	}
+}
